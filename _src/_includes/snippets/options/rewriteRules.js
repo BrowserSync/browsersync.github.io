388ed98deba8bed390eaa8b7c3aa064587be56b9
Greg Gamel <ggamel@users.noreply.github.com>
2015-05-20 15:39:25 +0100

    docs: update rewriteRules example - closes #17

--- _src/_includes/snippets/options/rewriteRules.js
+++ _src/_includes/snippets/options/rewriteRules.js
@@ -1,9 +1,9 @@
-// replace every occurence of the word BrowserSync with 'kittenz'
+// replace every occurrence of the word BrowserSync with 'kittenz'
 rewriteRules: [
     {
         match: /BrowserSync/g,
         fn: function (match) {
-            return "shane";
+            return 'kittenz';
         }
     }
 ]

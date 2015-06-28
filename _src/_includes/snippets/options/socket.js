fba22501720645125438af6d3f851fadb99254a2
Shane Osbourne <shane.osbourne8@gmail.com>
2015-06-28 14:59:34 +0100

    docs: Add clearer docs re: socket namespace/domain options

--- _src/_includes/snippets/options/socket.js
+++ _src/_includes/snippets/options/socket.js
@@ -1,8 +1,12 @@
-// This will result in something like this in
-// the socket connector code
-// browserSync.io('localhost:3003/browser-sync')
+// To change the namespace used by Browsersync
 socket: {
-    namespace: function (namespace) {
-        return "localhost:3003" + namespace;
-    }
+    namespace: '/someothername'
+}
+
+// By default, Browsersync is configured to use the domain
+// of your page in order to connect to socket.io
+// eg: location.host + '/browser-sync
+// but, should you need to change that, provide the `domain` property
+socket: {
+    domain: 'localhost:3000'
 }

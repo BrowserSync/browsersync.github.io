38d36c09d3d92a335d036f3131996e3781bd81d9
Shane Osbourne <shane.osbourne8@gmail.com>
2015-09-03 16:11:04 +0100

    docs: add documentation about new scroll options

---
+++ _src/_includes/snippets/options/scrollElementMapping.js
@@ -0,0 +1,3 @@
+// Sync the scrollTop position between different elements
+// useful if your scroll container changes based on a breakpoint
+scrollElementMapping: ['.scroller-mobile', '.scroller']
---
+++ _src/_includes/snippets/options/scrollElements.js
@@ -0,0 +1,3 @@
+// Sync the scrollTop position of the element .scroller
+// in addition to the window scroll
+scrollElements: ['.scroller']

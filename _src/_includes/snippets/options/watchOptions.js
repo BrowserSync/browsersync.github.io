afeea86010f0f5b525271ef6e3f4359b4f6e6cd4
Shane Osbourne <shane.osbourne8@gmail.com>
2016-09-15 19:43:26 +0100

    Update watchOptions snippet example

--- _src/_includes/snippets/options/watchOptions.js
+++ _src/_includes/snippets/options/watchOptions.js
@@ -1,19 +1,31 @@
+const bs = require('browser-sync').create();
+
 // Options passed to Chokidar
-watchOptions: {
-    ignoreInitial: true,
-    ignored: '*.txt'
-}
+bs.init({
+    watchOptions: {
+        ignoreInitial: true,
+        ignored: '*.txt'
+    },
+    files: ['./app']
+});
 
 // options for chokidar with custom callback
 // since 2.6.0
-files: [
-    {
-        match: ["wp-content/themes/**/*.php"],
-        fn: function (event, file) {
-            /** Custom event handler **/
-        },
-        options: {
-            ignored: '*.txt'
+bs.init({
+    files: [
+        {
+            match: ["wp-content/themes/**/*.php"],
+            fn: function (event, file) {
+                /** Custom event handler **/
+            },
+            options: {
+                ignored: '*.txt'
+            }
         }
-    }
-]
+    ]
+});
+
+// NOTE: the .watch() method will not receive
+// these options automatically, so you must provide
+// them manually in the following way
+bs.watch(['app/*.css'], {ignored: '*.map.css'});
